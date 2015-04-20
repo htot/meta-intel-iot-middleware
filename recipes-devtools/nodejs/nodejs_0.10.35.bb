htot/meta-intel-iot-middleware

commit 481b3eb9ec8c398b28872dd45dcd5662c7228b91

nodejs: fix openssl dependency
The DEPENDS_class-target *overrides* DEPENDS when compiling for the
target, despite depending on openssl in both cases. What must have
been intended is conditionally appending to DEPENDS.

Signed-off-by: Patrick Ohly <patrick.ohly@intel.com>
Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

recipes-devtools/nodejs/nodejs_0.10.35.bb
@@ -5,7 +5,7 @@ LICENSE = "MIT & BSD-2-Clause & BSD-3-Clause & BSD-4-Clause & ISC & GPLv2 & GPLv
 LIC_FILES_CHKSUM = "file://LICENSE;md5=e804bf1e856481415099460bc54d9316"
 
 DEPENDS = "openssl"
-DEPENDS_class-target = "nodejs-native"
+DEPENDS_append_class-target = "nodejs-native"
 
 SRC_URI = "http://nodejs.org/dist/v${PV}/node-v${PV}.tar.gz"
 

commit 675c49a2089a797874034fd50b4742eaa840ec7f

nodejs: fix spacing in DEPENDS
481b3eb9ec8c398b28872dd45dcd5662c7228b91 introduced an issue with DEPENDS
spacing being incorrect

Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

recipes-devtools/nodejs/nodejs_0.10.35.bb
@@ -5,7 +5,7 @@ LICENSE = "MIT & BSD-2-Clause & BSD-3-Clause & ISC & AFL-2.0 & Apache-2.0 & Open
 LIC_FILES_CHKSUM = "file://LICENSE;md5=e804bf1e856481415099460bc54d9316"
 
 DEPENDS = "openssl"
-DEPENDS_append_class-target = "nodejs-native"
+DEPENDS_append_class-target = " nodejs-native"
 
 SRC_URI = "http://nodejs.org/dist/v${PV}/node-v${PV}.tar.gz"
 